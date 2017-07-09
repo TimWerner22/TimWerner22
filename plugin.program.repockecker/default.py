1ad6a37c10a7d3ac3f3605bedc2599772abf546d
Kodiman1402 <admin@kodiman.eu>
2017-07-09 23:14:59 +0200

    repo checker update

--- plugin.program.repockecker/default.py
+++ plugin.program.repockecker/default.py
@@ -3,8 +3,9 @@
 import xbmcgui,requests
 
 urls = ["HSK Server 1*http://hsk.goip.de/hsk-repo/de/repository.hsk.kodiaddons/repository.hsk.kodiaddons-1.0.1.zip",
-       "HSK Server 2*http://v36557.1blu.de/files/public-docs/hsk/17/kodi_backup.zip",
-	   "Netzkino*http://www.netzkino.de/"]
+        "HSK Server 2*http://v36557.1blu.de/files/public-docs/hsk/17/kodi_backup.zip",
+	    "Netzkino*http://www.netzkino.de/",
+		"Kodimans Public Repo*https://github.com/Kodiman1402/repository.kodiman.public-1.0.2"]
 	   
 output = ''		
 for url in urls:
